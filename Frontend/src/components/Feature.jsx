e705ddbe1e4871cd66a6942dcd2223bb27ff4813
NGnYan <ngnyan@air-de-ngnyan.home>
2025-12-22 00:24:14 +0100

    feat(homepage): add HeroBanner, features and footer

---
+++ Frontend/src/components/Feature.jsx
@@ -0,0 +1,13 @@
+import "../styles/components/Feature.css";
+
+function Feature({ icon, title, text }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt="Feature Icon" className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Feature;
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,45 @@
 import "../styles/pages/HomePage.css";
 import Navbar from "../components/Navbar";
+import HeroBanner from "../components/HeroBanner";
+import Feature from "../components/Feature";
+import Footer from "../components/Footer";
+
+import chatIcon from "../assets/img/icon-chat.png";
+import moneyIcon from "../assets/img/icon-money.png";
+import securityIcon from "../assets/img/icon-security.png";
 
 function HomePage() {
   return (
-    <header>
-      <Navbar />
-    </header>
+    <section>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <HeroBanner />
+        <section className="features">
+          <Feature
+            icon={chatIcon}
+            title="You are our #1 priority"
+            text="Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes."
+          />
+          <Feature
+            icon={moneyIcon}
+            title="More savings means higher rates"
+            text="The more you save with us, the higher your interest rate will be!"
+          />
+          <Feature
+            icon={securityIcon}
+            title="Security you can trust"
+            text=" We use top of the line encryption to make sure your data and money
+            is always safe."
+          />
+        </section>
+        <footer>
+          <Footer />
+        </footer>
+      </main>
+    </section>
   );
 }
 
