9e911a971c5b2ec34b9d2e2412aba0614d3ead7e
NGnYan <ngnyan@air-de-ngnyan.home>
2025-12-23 00:15:10 +0100

    feat: implement login page layout

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,22 @@
+import "../styles/pages/LoginPage.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import LoginForm from "../components/LoginForm";
+
 function LoginPage() {
-  return <div></div>;
+  return (
+    <section className="login-page">
+      <header>
+        <Navbar />
+      </header>
+
+      <main className="main bg-dark">
+        <LoginForm />
+      </main>
+
+      <Footer />
+    </section>
+  );
 }
 
 export default LoginPage;
